53010c6552036c3a62ad2f7f3c561a6a6266417b
Howard Liu <rhowardliu@gmail.com>
2019-11-11 03:14:25 +0800

    Added head and leaves function for Cluster class

--- tree_parsing.py
+++ tree_parsing.py
@@ -5,31 +5,51 @@ class Cluster(object):
   """docstring for Cluster"""
   def __init__(self, clusterSet):
     super(Cluster, self).__init__()
-    self.cluster = []
+    self.cluster = self.sortClusterSet(clusterSet)
+    self.head = self.cluster[0]
+    self.leaves = self.getLeaves()
+
+
+  def getLeaves(self):
+    leaves = []
+    for node in cluster:
+      isLeaf = True
+      for child in node.children:
+        if child in self.cluster:
+          isLeaf = False
+      if isLeaf:
+        leaves.append(node)
+    return leaves
+
+
+  def sortClusterSet(self, clusterSet):
+    cluster = []
     for node in clusterSet:
-      if node.parent in self.cluster:
-        parent_index = self.cluster.index(node.parent)
-        self.cluster.insert(parent_index+1, node)
+      if node.parent in cluster:
+        parent_index = cluster.index(node.parent)
+        cluster.insert(parent_index+1, node)
         continue
       if node.children:
         if len(node.children) == 2:
-          if node.children[0] and node.children[1] in self.cluster:
-            bigger_index = max(self.cluster.index(node.children[0]), self.cluster.index(node. children[1]))
-            smaller_index = min(self.cluster.index(node.children[0]), self.cluster.index(node. children[1]))
-            bigchild = self.cluster[bigger_index]
-            self.cluster.remove(bigchild)
-            self.cluster.insert(smaller_index, bigchild)
-            self.cluster.insert(smaller_index, node)
+          if node.children[0] and node.children[1] in cluster:
+            bigger_index = max(cluster.index(node.children[0]), cluster.index(node. children[1]))
+            smaller_index = min(cluster.index(node.children[0]), cluster.index(node. children[1]))
+            bigchild = cluster[bigger_index]
+            cluster.remove(bigchild)
+            cluster.insert(smaller_index, bigchild)
+            cluster.insert(smaller_index, node)
             continue
-          elif node.children[1] in self.cluster:
-            child_index = self.cluster.index(node.children[0])
-            self.cluster.insert(child_index, node)
+          elif node.children[1] in cluster:
+            child_index = cluster.index(node.children[0])
+            cluster.insert(child_index, node)
             continue
-        if node.children[0] in self.cluster:
-          child_index = self.cluster.index(node.children[0])
-          self.cluster.insert(child_index, node)
+        if node.children[0] in cluster:
+          child_index = cluster.index(node.children[0])
+          cluster.insert(child_index, node)
           continue
-      self.cluster.append(node)
+      cluster.append(node)
+    return cluster
+
 
   def __str__(self):
     return ','.join(map(str, self.cluster))
