2c8ef0857c5603a4ecd22c5d56c8dc7677bd63a0
Howard Liu <rhowardliu@gmail.com>
2019-11-14 18:17:30 +0800

    app is now OODP

--- control.py
+++ control.py
@@ -1,41 +1,30 @@
 import psycopg2
 import tree_parsing as tp
-import db_connect as db
+from db_connect import DB_Manager
 import vocalizer as v
 
-def add_explain_analyze(query):
-  return 'EXPLAIN (ANALYZE true, FORMAT json) ' + query
-
-def get_query_plan(cur,query):
-  cur.execute(query)
-  res = cur.fetchall()
-  return res[0][0][0]
-
-def generate_differences(query_1, query_2):
-  conn = db.get_sql_cursor()
-  cur = conn.cursor()
-  p_1_json = get_query_plan(cur, add_explain_analyze(query_1))
-  p_2_json = get_query_plan(cur, add_explain_analyze(query_2))
-  p_1 = v.parse_json(p_1_json)
-  p_2 = v.parse_json(p_2_json)
-  clusters = tp.parse_tree(p_1, p_2)
-  cluster_dict = tp.create_cluster_dict(clusters)
-  diff_1 = tp.get_tree_differences(p_1,cluster_dict)
-  diff_2 = tp.get_tree_differences(p_2,cluster_dict)
-  print(tp.print_tree_difference(diff_1, diff_2))
-  cur.close()
-  conn.close()
+class Control(object):
+  """docstring for Control"""
+  def __init__(self, ):
+    super(Control, self).__init__()
+    self.db = DB_Manager()
+    
+  def add_explain_analyze(self, query):
+    return 'EXPLAIN (ANALYZE true, FORMAT json) ' + query
 
 
-
-# self.pushButton.clicked.connect(self.onClickButton)
-
-# def onClickButton(self):
-#     query = self.plainTextEdit.toPlainText()
-#     tree = qp.query_to_tree(query)
-#     self.textBrowser.setText(tree)
-#     print(query)
-
+  def generate_differences(self, query_1, query_2):
+    p_1_json = self.db.query(self.add_explain_analyze(query_1))
+    p_2_json = self.db.query(self.add_explain_analyze(query_2))
+    p_1 = v.parse_json(p_1_json)
+    p_2 = v.parse_json(p_2_json)
+    clusters = tp.parse_tree(p_1, p_2)
+    cluster_dict = tp.create_cluster_dict(clusters)
+    diff_1 = tp.get_tree_differences(p_1,cluster_dict)
+    diff_2 = tp.get_tree_differences(p_2,cluster_dict)
+    diff_statement = tp.print_tree_difference(diff_1, diff_2)
+    print(diff_statement)
+    return diff_statement
 
 
 if __name__ == '__main__':
@@ -64,7 +53,7 @@ order by
 limit 10;
 '''
 
-test_2 = '''select
+  test_2 = '''select
   l_orderkey,
   sum(l_extendedprice * (1 - l_discount)) as revenue,
   o_orderdate,
@@ -87,4 +76,5 @@ order by
 limit 10;
 
 '''
-generate_differences(test_1, test_2)
+  ctrl = Control()
+  ctrl.generate_differences(test_1, test_2)
--- db_connect.py
+++ db_connect.py
@@ -1,10 +1,22 @@
 import psycopg2
 import configparser
 
-def get_sql_cursor():
-  config = configparser.ConfigParser()
-  config.read('./config.ini')
-  pg_config = config['postgres']
-  return psycopg2.connect(host = pg_config['host'], database = pg_config['database'], \
-                           user = pg_config['user'], password = pg_config['password'])
+class DB_Manager(object):
+  """docstring for db_manager"""
+  def __init__(self):
+    super(DB_Manager, self).__init__()
+    config = configparser.ConfigParser()
+    config.read('./config.ini')
+    pg_config = config['postgres']
+    self.conn = psycopg2.connect(host = pg_config['host'], database = pg_config['database'], \
+                             user = pg_config['user'], password = pg_config['password'])
+    self.cur = self.conn.cursor()
+    print('db connection successful')
+
+  def query(self, query):
+    print('querying...')
+    self.cur.execute(query)
+    print('query successful')
+    res = self.cur.fetchall()
+    return res[0][0][0]
 
